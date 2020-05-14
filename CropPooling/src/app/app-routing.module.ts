292601b97b54edefc28d3c882770db9b6a4f5592
IMSRIJAN04 <srijanerroju@gmail.com>
2020-05-14 18:52:31 +0530

    finished authentication modi

--- CropPooling/src/app/app-routing.module.ts
+++ CropPooling/src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
-import {from} from 'rxjs';
 import { SignupComponent } from './signup/signup.component';
-import { EmailComponent } from './email/email.component';
 import { MembersComponent } from './members/members.component';
-import { AuthGuard } from './core/auth.service';
+//importing authguard after creating it
+import { AuthGuard } from './core/guard/auth.guard';
 
 const routes: Routes = [
   {path:'',component:WelcomeComponent},
-  {path:'login',component:LoginComponent,canActivate:[AuthGuard]},
-  {path:'signup',component:SignupComponent,canActivate:[AuthGuard]},
-  {path:'login-email',component:EmailComponent,canActivate:[AuthGuard]},
-  {path:'members',component:MembersComponent, resolve:{data: UserResolver}},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'members',component:MembersComponent,canActivate: [AuthGuard]},
   {path:'about',component:AboutComponent}
   
 
--- CropPooling/src/app/members/members.component.ts
+++ CropPooling/src/app/members/members.component.ts
@@ -1,35 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, AuthProviders, AuthMethods } from 'angularfire';
 import { Router } from '@angular/router';
-import { moveIn, fallIn, moveInLeft } from '../router.animations';
+
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.scss'],
-  animations: [moveIn(), fallIn(), moveInLeft()],
-  host: {'[@moveIn]': ''}
 })
 export class MembersComponent implements OnInit {
 
-  name: any;
-  state: string = '';
-
-  constructor(public af: AngularFire,private router: Router) {
-
-    this.af.auth.subscribe(auth => {
-      if(auth) {
-        this.name = auth;
-      }
-    });
-
+  constructor() {
   }
-
-  logout() {
-     this.af.auth.logout();
-     console.log('logged out');
-     this.router.navigateByUrl('/login');
-  }
-
   
   ngOnInit(): void {
   }
