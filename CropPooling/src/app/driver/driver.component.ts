c5ea4776c467535cd972e3ad269f7e9abacc923e
IMSRIJAN04 <srijanerroju@gmail.com>
2020-05-19 16:44:56 +0530

    add truck styling

--- CropPooling/src/app/driver/driver.component.ts
+++ CropPooling/src/app/driver/driver.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,NgZone} from '@angular/core';
+import {AuthService } from 'src/app/core/services/auth.service';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-driver',
   templateUrl: './driver.component.html',
   styleUrls: ['./driver.component.scss']
 })
 export class DriverComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    public ngZone: NgZone
+  ) { }
 
   ngOnInit(): void {
   }
