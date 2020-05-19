5836bbac10869329db3fa93219e686bebf260ba1
IMSRIJAN04 <srijanerroju@gmail.com>
2020-05-19 12:48:29 +0530

    working on edit truck

--- CropPooling/src/app/driver/truck/edit-truck/edit-truck.component.ts
+++ CropPooling/src/app/driver/truck/edit-truck/edit-truck.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CrudService } from 'src/app/driver/shared/crud.service';
+import { ActivatedRoute, Router } from "@angular/router"; // ActivatedRoue is used to get the current associated components information.
+import { Location } from '@angular/common';  // Location service is used to go back to previous component
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-edit-truck',
